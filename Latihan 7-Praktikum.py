d8ddd7d116b7ab6aebb1d34f6149a86304849060
AlphaJr1 <113828544+AlphaJr1@users.noreply.github.com>
2022-10-07 14:39:35 +0700

    Add files via upload

---
+++ Latihan 7-Praktikum.py
@@ -0,0 +1,10 @@
+# Adrian ALfajri - 064002200009
+
+baris = int(input('Masukkan Jumlah Maksimal : '))
+
+for i in range(baris, 0, -1):
+    for j in range(1, i+1):
+        print(i, end=" ")
+        
+    print("\n")
+
